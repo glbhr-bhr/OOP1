e26b33c0572fb5935c46caad063b374a3d208849
glbhr-bhr <73697433+glbhr-bhr@users.noreply.github.com>
2021-01-21 00:38:25 +0300

    Proje dosyası ekle.

---
+++ Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        public int Id { get; set; }
+
+        //Kategori
+        public int CategoryId { get; set; }
+
+        //ürün ismi
+        public string ProductName { get; set; }
+
+        //birim fiyatı
+        public double UnitPrice { get; set; }
+
+        //stok adeti
+        public int UnitInStock { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product() { };
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.UnitPrice = 100;
+            product1.UnitInStock = 3;
+
+            Product product2 = new Product { Id = 2, 
+                CategoryId = 4,
+                UnitPrice = 35, 
+                UnitInStock = 50,
+                ProductName="kitap" };
+
+
+            //PascalCase    //camelCase
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+           
+
+
+        }
+    }
+}
